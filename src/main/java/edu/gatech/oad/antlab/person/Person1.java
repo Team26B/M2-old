36ba52d3ab10b7fc60535f63f25cdc42550b7c3c
Brian Zhu <bzhu75@gatech.edu>
2019-01-21 19:01:20 -0500

    finished string manipulation method

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,14 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+        String toReturn = "";
+        int real = 0;
+
+        for (int i = 2; i < input.length() + 3; i++) {
+            real = i % input.length();
+            toReturn = toReturn + input.charAt(real);
+        }
+	    return input;
 	}
 	
 	/**
